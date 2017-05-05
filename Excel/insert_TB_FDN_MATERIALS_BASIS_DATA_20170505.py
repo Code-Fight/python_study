a3da619f7beb335028302b52ee8b2ec4e4f7a05d
FengZhang <97576419@qq.com>
2017-05-05 13:26:13 +0800

    2017年4月13日16:59:40

---
+++ Excel/insert_TB_FDN_MATERIALS_BASIS_DATA_20170505.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# Author zfCode
+import xlrd
+import datetime
+from Oracle_helper import oracle_helper
+
+h=oracle_helper('dsjky/quickhigh@192.168.2.105:1521/DSJKY')
+data= xlrd.open_workbook('D:\公司文档\客运物资管理信息系统\物资系统2.0\报表补充170504\锦州.xls')
+table = data.sheets()[0]
+# print(len(data.sheets()))
+nrows = table.nrows
+ncols = table.ncols
+print(nrows,ncols)
+for i in range(nrows):
+    # print(table.row_values(i))
+    # print(xlrd.xldate.xldate_as_datetime(table.row_values(i)[21], 0))
+    # continue
+
+    print(table.row_values(i)[28])
+    print(i)
+    # ccrq= xlrd.xldate.xldate_as_datetime(table.row_values(i)[20], 0).strftime('%Y-%m-%d') if len(str(table.row_values(i)[20]).replace('\xa0', '').strip()) > 0 else ""
+    # ccrq
+
+
+    sql='''Insert into DSJKY.TB_FDN_MATERIALS_BASIS_DATA (
+        DW,CJ,CLKMC,WZBH,WZMC,GGXH,JLDW,KCDJ,QC_COUNT,SR_COUNT,
+        ZC_COUNT,QM_COUNT,QC_MONEY,SR_MONEY,ZC_MONEY,QM_MONEY,SCCJ,CCRQ,ZBQN,SMQXN,
+        CLFL,STATE,MAX_CBDL,MIN_CBDL,WZSX,BFSJ,BFGZ,TIME)
+VALUES (N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s',
+        N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s',to_date('%s','yyyy-MM-dd'), N'%s',N'%s',
+        N'%s', N'%s', N'%s', N'%s', N'%s',to_date('%s','yyyy-MM-dd'), N'%s', to_date('%s','yyyyMM') )
+    '''%(str(table.row_values(i)[17]).replace('\xa0','').strip()
+                   , str(table.row_values(i)[18]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[2]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[4]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[5]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[6]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[7]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[8]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[9]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[11]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[13]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[15]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[10]).replace('\xa0', '').strip()
+                   , str(table.row_values(i)[12]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[14]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[16]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[19]).replace('\xa0', '').strip()
+                 , xlrd.xldate.xldate_as_datetime(table.row_values(i)[20], 0).strftime('%Y-%m-%d') if len(str(table.row_values(i)[20]).replace('\xa0', '').strip())>0 else ""
+
+                 , str(table.row_values(i)[21]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[22]).replace('\xa0', '').strip()
+
+                , str(table.row_values(i)[23]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[24]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[25]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[26]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[27]).replace('\xa0', '').strip()
+                 ,xlrd.xldate.xldate_as_datetime(table.row_values(i)[28], 0).strftime('%Y-%m-%d') if len(str(table.row_values(i)[28]).replace('\xa0', '').strip())>0 else ""
+                 , str(table.row_values(i)[29]).replace('\xa0', '').strip()
+                 , str(table.row_values(i)[3]).replace('\xa0', '').strip()
+         )
+
+
+    # sql = '''
+    # INSERT INTO TB_FDN_MATERIALS_BASIS_DATA (DW, CLKMC, WZBH, WZMC, GGXH,JLDW,KCDJ,QC_COUNT,SR_COUNT,ZC_COUNT,QM_COUNT,QC_MONEY,SR_MONEY,ZC_MONEY,QM_MONEY)
+    # VALUES (N'吉林客运段', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s', N'%s')
+    # '''%(str(table.row_values(i)[2]).replace('\xa0','').strip()
+    #                , str(table.row_values(i)[4]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[5]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[6]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[7]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[8]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[9]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[11]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[13]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[15]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[10]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[12]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[14]).replace('\xa0', '').strip()
+    #                , str(table.row_values(i)[16]).replace('\xa0', '').strip()
+    #      )
+    print(sql)
+    h.executeNoquery(sql)
+    # print(table.row_values(i))
